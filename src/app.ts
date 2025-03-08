f753bdb0ae2adea4f591d4f153a29e531c6134c1
Dylan Marcano <kevinmarcano@gmail.com>
2025-03-08 13:17:55 -0400

    reestructure backend

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+const app: express.Application = express();
+
+// Tipo para el puerto
+type Port = number | string;
+
+
+// Rutas
+import routerAPI from './routes/routerAPI';
+app.use('/api', routerAPI);
+
+import routerSendEmail from './routes/routerSendEmail';
+app.use('/api/send-email', routerSendEmail);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hola mundo");
+});
+
+const PORT: Port = process.env.PORT  || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`);
+});
