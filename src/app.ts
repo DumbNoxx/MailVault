6df70b3bd485613ff4b3e01a10f7a4f3cab6ece6
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-20 13:55:51 -0400

    add temporal cors, i not have network

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const app: express.Application = express();
 
 // Import type for port
 import { Port } from "./types/port.type";
+
+app.use((req: Request, res: Response, next:NextFunction): void => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        res.status(200);
+    }
+
+    next();
+});
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
 
 // Routes
 import routerAPI from './routes/routerAPI';
