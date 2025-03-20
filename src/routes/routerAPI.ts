632b2092687b23ff3cdde105b533bcb09298e6db
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-20 07:24:29 -0400

    Empezar con la documentacion 1.0.1

--- src/routes/routerAPI.ts
+++ src/routes/routerAPI.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-
 const routerAPI: express.Router = express.Router();
+
 
 routerAPI.get('/', (req: Request, res: Response ) => {
     res.send("Saludo desde la ruta /api");
