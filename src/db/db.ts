b6d5ec76ddc344192a6a3c3b206403e365f6402a
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-21 00:19:58 -0400

    aggreed data base

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,16 +3,17 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
+const db_name = process.env.DB_NAME;
 
 /**
  * Use your data such as the user of your database
  * and password, also the host
  */
 const pool: mysql.Pool = mysql.createPool({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "emails",
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -22,8 +23,12 @@ const pool: mysql.Pool = mysql.createPool({
 
 const initializeDatabase = async () => {
   try {
+    if (!db_name){
+      throw new Error('The db_name is not defined'); 
+    }
     const sqFilePath = path.join(__dirname, "db.sql");
-    const sqFileContent = fs.readFileSync(sqFilePath, "utf-8");
+    let sqFileContent = fs.readFileSync(sqFilePath, "utf-8");
+    sqFileContent = sqFileContent.replace(/\$\{DB_NAME\}/g, db_name);
     // console.log("Content of file SQL:", sqFileContent); // debugging
     const connection = await pool.getConnection();
     const queries = sqFileContent
